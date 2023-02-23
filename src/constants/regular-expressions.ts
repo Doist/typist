6950afb61bd22a3b029e7f688f5e704402290e5a
Ricardo Amaral <ricardo@doist.com>
2023-02-23 13:08:23 +0000

    fix(markdown-serializer): Override Turndown escaping behaviour with custom rules (#102)

--- src/constants/regular-expressions.ts
+++ src/constants/regular-expressions.ts
@@ -3,4 +3,11 @@
  */
 const REGEX_LINE_BREAKS = /(?:\r\n|\r|\n)/g
 
-export { REGEX_LINE_BREAKS }
+/**
+ * A regex for standard punctuation characters for US-ASCII plus unicode punctuation.
+ *
+ * @see https://stackoverflow.com/a/25575009
+ */
+const REGEX_PUNCTUATION = /[\u2000-\u206f\u2e00-\u2e7f'!"#$%&()*+,\-./:;<=>?@\\[\]^_`{|}~]/
+
+export { REGEX_LINE_BREAKS, REGEX_PUNCTUATION }
