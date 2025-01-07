6673d83e1e6985cfcd68e5f836518f7edee8d444
Ricardo Amaral <ricardo@doist.com>
2025-01-07 16:15:47 +0000

    fix(rich-text-code): Retore original Tiptap input/paste rules (#999)

--- src/extensions/rich-text/rich-text-code.ts
+++ src/extensions/rich-text/rich-text-code.ts
@@ -1,3 +1,4 @@
+import { markInputRule, markPasteRule } from '@tiptap/core'
 import { Code } from '@tiptap/extension-code'
 
 import { CODE_EXTENSION_PRIORITY } from '../../constants/extension-priorities'
@@ -10,6 +11,18 @@ import type { CodeOptions } from '@tiptap/extension-code'
 type RichTextCodeOptions = CodeOptions
 
 /**
+ * The original input regex for Markdown inline code (i.e. `<code>code</code>`) to prevent the issue
+ * introduced in this PR: https://github.com/ueberdosis/tiptap/pull/4468#issuecomment-2575093998
+ */
+const inputRegex = /(?:^|\s)(`(?!\s+`)((?:[^`]+))`(?!\s+`))$/
+
+/**
+ * The original paste regex for Markdown inline code (i.e. `<code>code</code>`) to prevent the issue
+ * introduced in this PR: https://github.com/ueberdosis/tiptap/pull/4468#issuecomment-2575093998
+ */
+const pasteRegex = /(?:^|\s)(`(?!\s+`)((?:[^`]+))`(?!\s+`))/g
+
+/**
  * Custom extension that extends the built-in `Code` extension to allow all marks (e.g., Bold,
  * Italic, and Strikethrough) to coexist with the `Code` mark (as opposed to disallowing all any
  * other mark by default).
@@ -20,6 +33,22 @@ type RichTextCodeOptions = CodeOptions
 const RichTextCode = Code.extend<RichTextCodeOptions>({
     priority: CODE_EXTENSION_PRIORITY,
     excludes: Code.name,
+    addInputRules() {
+        return [
+            markInputRule({
+                find: inputRegex,
+                type: this.type,
+            }),
+        ]
+    },
+    addPasteRules() {
+        return [
+            markPasteRule({
+                find: pasteRegex,
+                type: this.type,
+            }),
+        ]
+    },
 })
 
 export { RichTextCode }
