f7a31e924b7f50008bd913ea841f473f95955311
Ricardo Amaral <ricardo@doist.com>
2026-04-20 11:54:16 +0100

    chore: replace husky and lint-staged with lefthook (#1304)

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    './**/*.{js,jsx,ts,tsx,mts,css,json,jsonc,md,yml,yaml,html,svg}': [
-        'oxfmt --no-error-on-unmatched-pattern',
-    ],
-    './**/*.{js,jsx,ts,tsx,mts}': ['oxlint --fix'],
-}
